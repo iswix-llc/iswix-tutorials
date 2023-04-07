9105671a980ec76a4830ae1d7cfa5691ea59001e
Christopher Painter <chrpai@iswix.com>
2023-04-07 07:42:03 -0500

    Initial Checkin of WebAPI Tutorial

---
+++ WiX-v4-HeatWave/web-api/Application/Api/Program.cs
@@ -0,0 +1,32 @@
+
+namespace Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
